6e902178f07ecd7ad97f54af1888a25225f23271
Andrey Tarantsov <andreyvit@gmail.com>
2012-04-21 17:04:10 +0700

    Use a more unique callback name in fslistener_osx

--- src/fslistener_osx.c
+++ src/fslistener_osx.c
@@ -7,7 +7,8 @@
 
 
 
-static void FSMonitorEventStreamCallback(ConstFSEventStreamRef streamRef, fslistener_t *listener, size_t numEvents, char *eventPaths[], const FSEventStreamEventFlags eventFlags[], const FSEventStreamEventId eventIds[]);
+static void fslistener_FSMonitorEventStreamCallback(ConstFSEventStreamRef streamRef, fslistener_t *listener, size_t numEvents, char *eventPaths[], const FSEventStreamEventFlags eventFlags[], const FSEventStreamEventId eventIds[]);
+
 
 
 fslistener_t *fslistener_create(const char *path, fslistener_callback_t callback, void *data) {
@@ -28,7 +29,7 @@ fslistener_t *fslistener_create(const char *path, fslistener_callback_t callback
   CFArrayRef paths = CFArrayCreate(NULL, (const void **)&pathRef, 1, &kCFTypeArrayCallBacks);
   CFRelease(pathRef);
 
-  listener->streamRef = FSEventStreamCreate(NULL, (FSEventStreamCallback)FSMonitorEventStreamCallback, &context, paths, kFSEventStreamEventIdSinceNow, 0.05, 0);
+  listener->streamRef = FSEventStreamCreate(NULL, (FSEventStreamCallback)fslistener_FSMonitorEventStreamCallback, &context, paths, kFSEventStreamEventIdSinceNow, 0.05, 0);
   assert(listener->streamRef && "FSEventStreamCreate failed");
 
   CFRelease(paths);
@@ -54,7 +55,7 @@ void fslistener_free(fslistener_t *listener) {
   free(listener);
 }
 
-void FSMonitorEventStreamCallback(ConstFSEventStreamRef streamRef, fslistener_t *listener, size_t numEvents, char *eventPaths[], const FSEventStreamEventFlags eventFlags[], const FSEventStreamEventId eventIds[]) {
+static void fslistener_FSMonitorEventStreamCallback(ConstFSEventStreamRef streamRef, fslistener_t *listener, size_t numEvents, char *eventPaths[], const FSEventStreamEventFlags eventFlags[], const FSEventStreamEventId eventIds[]) {
   for (size_t i = 0; i < numEvents; ++i) {
     // TODO: check eventFlags[i] to give a better hint
     listener->callback(eventPaths[i], fslistener_hint_dir_deep, listener->callback_data);
